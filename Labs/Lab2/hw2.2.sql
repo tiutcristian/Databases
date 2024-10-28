30675c206bcff6cef15435a6a458b6d28eaf2a33
Cristian Tiut <tiutcristian@gmail.com>
2024-10-28 11:46:48 +0200

    some progress to lab2

--- Labs/Lab2/hw2.2.sql
+++ Labs/Lab2/hw2.2.sql
@@ -9,3 +9,21 @@ VALUES  (1, 1, 1000, '2020-01-01', '2020-12-31'),
 -- Nike sponsors 1 and 2
 -- Adidas sponsors 2 and 3
 -- Puma sponsors 1 and 3 (previously inserted)
+
+-- Insert a new event
+INSERT INTO Events (name, [location]) VALUES ('Event 1', 'Location 1');
+
+-- Insert a new swim pool
+INSERT INTO SwimPools (name, [location], [length], [width], [depth]) VALUES ('Pool 1', 'Location 1', 50, 25, 3);
+
+-- Insert a new bike track
+INSERT INTO BikeTracks (name, [location], [length], [difficulty]) VALUES ('Bike Track 1', 'Location 1', 100, 'easy');
+
+-- Insert a new running track
+INSERT INTO RunningTracks (name, [location], [length], [difficulty]) VALUES ('Running Track 1', 'Location 1', 100, 'easy');
+
+-- Insert a new race
+INSERT INTO Races (name, [date], swim_pool_id, bike_track_id, run_track_id, event_id)
+VALUES ("race1", "2020-01-01", 1, 1, 1, 1);
+
+-- !do some inserts in the race results for race 1 and some athletes
--- Labs/Lab2/hw2.3.sql
+++ Labs/Lab2/hw2.3.sql
@@ -29,5 +29,99 @@
             SELECT id
             FROM Sponsors
             WHERE name = 'Adidas' OR name = 'Nike'
+        ) 
+    )
+
+
+
+-- b. 2 queries with the intersection operation; use INTERSECT and IN;
+    -- Select IDs of athletes that are sponsored by both Adidas and Nike
+    -- 1. With INTERSECT
+    SELECT athlete_id
+    FROM Sponsorships
+    WHERE sponsor_id = (
+        SELECT id
+        FROM Sponsors
+        WHERE name = 'Adidas'
+    )
+
+    INTERSECT
+
+    SELECT athlete_id
+    FROM Sponsorships
+    WHERE sponsor_id = (
+        SELECT id
+        FROM Sponsors
+        WHERE name = 'Nike'
+    )
+
+    -- 2. With IN
+    SELECT athlete_id
+    FROM Sponsorships
+    WHERE sponsor_id = (
+        SELECT id
+        FROM Sponsors
+        WHERE name = 'Adidas'
+    ) AND athlete_id IN (
+        SELECT athlete_id
+        FROM Sponsorships
+        WHERE sponsor_id = (
+            SELECT id
+            FROM Sponsors
+            WHERE name = 'Nike'
         )
     )
+
+
+
+-- c. 2 queries with the difference operation; use EXCEPT and NOT IN;
+    -- Select IDs of athletes that are sponsored by Adidas but not by Nike
+    -- 1. With EXCEPT
+    SELECT athlete_id
+    FROM Sponsorships
+    WHERE sponsor_id = (
+        SELECT id
+        FROM Sponsors
+        WHERE name = 'Adidas'
+    )
+
+    EXCEPT
+
+    SELECT athlete_id
+    FROM Sponsorships
+    WHERE sponsor_id = (
+        SELECT id
+        FROM Sponsors
+        WHERE name = 'Nike'
+    )
+
+    -- 2. With NOT IN
+    SELECT athlete_id
+    FROM Sponsorships
+    WHERE sponsor_id = (
+        SELECT id
+        FROM Sponsors
+        WHERE name = 'Adidas'
+    ) AND athlete_id NOT IN (
+        SELECT athlete_id
+        FROM Sponsorships
+        WHERE sponsor_id = (
+            SELECT id
+            FROM Sponsors
+            WHERE name = 'Nike'
+        )
+    )
+
+
+
+-- d. 4 queries with INNER JOIN, LEFT JOIN, RIGHT JOIN, and FULL JOIN (one query per operator); 
+-- one query will join at least 3 tables, while another one will join at least two many-to-many relationships;
+
+-- INNER JOIN + many-to-many (Sponsorships, RaceResults)
+-- Find the athletes (id+name) sponsored by Nike that participated in race with id 1
+SELECT A.id, A.name
+FROM Athletes A
+JOIN Sponsorships SS ON A.id = SS.athlete_id
+JOIN Sponsors S ON SS.sponsor_id = S.id
+JOIN RaceResults RR ON A.id = RR.athlete
+WHERE S.name = 'Nike' AND RR.race_id = 1;
