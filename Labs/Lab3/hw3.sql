6aa5b6092d9f3ec8e13c8bc4b4042df10976a123
Cristian Tiut <tiutcristian@gmail.com>
2024-11-17 17:50:03 +0200

    hw 3 finished

--- Labs/Lab3/hw3.sql
+++ Labs/Lab3/hw3.sql
@@ -10,9 +10,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_ModifyColumn 'RaceResults', 'total_time', 'DECIMAL(10,2)'
-EXEC sp_ModifyColumn 'RaceResults', 'total_time', 'INT'
 GO
 
 --------------------------------------------
@@ -29,8 +26,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_AddColumn 'Teams', 'coloana_noua', 'INT'
 GO
 
 --------------------------------------------
@@ -46,8 +41,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_RemoveColumn 'Teams', 'coloana_noua'
 GO
 
 --------------------------------------------
@@ -67,8 +60,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_AddDefaultConstraint 'RaceResults', '0', 'total_time'
 GO
 
 --------------------------------------------
@@ -85,13 +76,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_RemoveDefaultConstraint 'RaceResults', 'total_time'
-GO
-
---------------------------------------------
-
-EXEC sp_CreateTable NewTable, 'id INT NOT NULL, name NVARCHAR(128)'
 GO
 
 --------------------------------------------
@@ -109,8 +93,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_AddPrimaryKey NewTable, id
 GO
 
 --------------------------------------------
@@ -127,8 +109,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_RemovePrimaryKey NewTable, id
 GO
 
 --------------------------------------------
@@ -146,8 +126,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_AddCandidateKey NewTable, id
 GO
 
 --------------------------------------------
@@ -164,8 +142,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_RemoveCandidateKey NewTable, id
 GO
 
 --------------------------------------------
@@ -185,8 +161,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_AddForeignKey NewTable, id, Teams, id
 GO
 
 --------------------------------------------
@@ -203,8 +177,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_RemoveForeignKey NewTable, id
 GO
 
 --------------------------------------------
@@ -220,8 +192,6 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_CreateTable NewTable, 'id INT NOT NULL, name NVARCHAR(128)'
 GO
 
 --------------------------------------------
@@ -236,13 +206,113 @@ BEGIN
 
     EXEC sp_executesql @sql
 END
-
-EXEC sp_DropTable NewTable
 GO
 
 --------------------------------------------
 
-EXEC sp_DropTable NewTable
+CREATE TABLE SchemaVersion (
+    Id INT PRIMARY KEY CHECK (Id = 1),
+    Version INT NOT NULL
+);
+INSERT INTO SchemaVersion (Id, Version) VALUES (1, 1);
 GO
 
 --------------------------------------------
+
+CREATE TABLE VersionToScriptMapping (
+    prev_version INT,
+    target_version INT NOT NULL,
+    Script NVARCHAR(MAX) NOT NULL,
+    PRIMARY KEY (prev_version, target_version)
+);
+
+INSERT INTO VersionToScriptMapping VALUES (1, 2, 'sp_CreateTable ''NewTable'', ' +
+    '''id INT NOT NULL, ' + 
+    'col NVARCHAR(128), ' +
+    'def_col INT, ' +
+    'cand_col INT NOT NULL, ' +
+    'foreign_col INT NOT NULL'''
+);
+INSERT INTO VersionToScriptMapping VALUES (2, 3, 'sp_ModifyColumn ''NewTable'', ''col'', ''INT''');
+INSERT INTO VersionToScriptMapping VALUES (3, 4, 'sp_AddColumn ''NewTable'', ''new_col'', ''INT''');
+INSERT INTO VersionToScriptMapping VALUES (4, 5, 'sp_AddDefaultConstraint ''NewTable'', ''0'', ''def_col''');
+INSERT INTO VersionToScriptMapping VALUES (5, 6, 'sp_AddPrimaryKey ''NewTable'', ''id''');
+INSERT INTO VersionToScriptMapping VALUES (6, 7, 'sp_AddCandidateKey ''NewTable'', ''cand_col''');
+INSERT INTO VersionToScriptMapping VALUES (7, 8, 'sp_AddForeignKey ''NewTable'', ''foreign_col'', ''Teams'', ''id''');
+
+INSERT INTO VersionToScriptMapping VALUES (2, 1, 'sp_DropTable ''NewTable''');
+INSERT INTO VersionToScriptMapping VALUES (3, 2, 'sp_ModifyColumn ''NewTable'', ''col'', ''NVARCHAR(128)''');
+INSERT INTO VersionToScriptMapping VALUES (4, 3, 'sp_RemoveColumn ''NewTable'', ''new_col''');
+INSERT INTO VersionToScriptMapping VALUES (5, 4, 'sp_RemoveDefaultConstraint ''NewTable'', ''def_col''');
+INSERT INTO VersionToScriptMapping VALUES (6, 5, 'sp_RemovePrimaryKey ''NewTable'', ''id''');
+INSERT INTO VersionToScriptMapping VALUES (7, 6, 'sp_RemoveCandidateKey ''NewTable'', ''cand_col''');
+INSERT INTO VersionToScriptMapping VALUES (8, 7, 'sp_RemoveForeignKey ''NewTable'', ''foreign_col''');
+GO
+
+--------------------------------------------
+
+CREATE OR ALTER PROCEDURE MigrateToVersion
+    @TargetVersion INT
+AS
+BEGIN
+    DECLARE @CurrentVersion INT;
+
+    SELECT @CurrentVersion = Version FROM SchemaVersion WHERE Id = 1;
+
+    WHILE @CurrentVersion < @TargetVersion
+    BEGIN
+        DECLARE @Script NVARCHAR(MAX);
+
+        SELECT @Script = Script 
+        FROM VersionToScriptMapping 
+        WHERE prev_version = @CurrentVersion AND target_version = @CurrentVersion + 1;
+
+        PRINT @Script; -- for debugging purposes
+    
+        EXEC sp_executesql @Script;
+
+        SET @CurrentVersion = @CurrentVersion + 1;
+
+        UPDATE SchemaVersion SET Version = @CurrentVersion;
+    END
+
+    WHILE @CurrentVersion > @TargetVersion
+    BEGIN
+        DECLARE @Script2 NVARCHAR(MAX);
+
+        SELECT @Script2 = Script
+        FROM VersionToScriptMapping
+        WHERE prev_version = @CurrentVersion AND target_version = @CurrentVersion - 1;
+
+        PRINT @Script2; -- for debugging purposes
+        
+        EXEC sp_executesql @Script2;
+
+        SET @CurrentVersion = @CurrentVersion - 1;
+        
+        UPDATE SchemaVersion SET Version = @CurrentVersion;
+    END
+END;
+
+--------------------------------------------
+
+-- Test
+EXEC MigrateToVersion @TargetVersion = 1;
+EXEC MigrateToVersion @TargetVersion = 2;
+EXEC MigrateToVersion @TargetVersion = 3;
+EXEC MigrateToVersion @TargetVersion = 4;
+EXEC MigrateToVersion @TargetVersion = 5;
+EXEC MigrateToVersion @TargetVersion = 6;
+EXEC MigrateToVersion @TargetVersion = 7;
+EXEC MigrateToVersion @TargetVersion = 8;
+
+--------------------------------------------
+
+-- V1 = no table
+-- V2 = table is created, col is VARCHAR
+-- V3 = col is INT, no new_col
+-- V4 = new_col is added, def_col has no default constraint
+-- V5 = def_col has default constraint, no primary key
+-- V6 = primary key is added, no candidate key
+-- V7 = candidate key is added, no foreign key
+-- V8 = foreign key is added
